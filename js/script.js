2e38589109cd3dba0ad986c43648a26485ecf37d
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-12-15 21:23:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/* Hg */
+document.getElementById('Hg-link').onmouseover = function() {
+    document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+}
+document.getElementById('Hg-link').onmouseout = function() {
+    document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+}
+/* Pb */
+document.getElementById('Pb-link').onmouseover = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+}
+document.getElementById('Pb-link').onmouseout = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+}
+/* Mn */
+document.getElementById('Mn-link').onmouseover = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+}
+document.getElementById('Mn-link').onmouseout = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+}
+/* Cd */
+document.getElementById('Cd-link').onmouseover = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
+}
+document.getElementById('Cd-link').onmouseout = function() {
+    document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
+}
