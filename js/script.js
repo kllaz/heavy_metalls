bdb2dba7ffe71018d4dea187f4d9ea985f8dd3c5
K_Laz <113620524+kllaz@users.noreply.github.com>
2022-12-16 11:04:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,42 +3,64 @@ document.getElementById('Hg-link').onmouseover = function() {
     document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+    document.getElementById('back').style.borderBottom = '0vw solid white';
 }
 document.getElementById('Hg-link').onmouseout = function() {
     document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('back').style.borderBottom = '0.5vw solid white';
 }
 /* Pb */
 document.getElementById('Pb-link').onmouseover = function() {
     document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+    document.getElementById('back').style.borderBottom = '0vw solid white';
 }
 document.getElementById('Pb-link').onmouseout = function() {
     document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('back').style.borderBottom = '0.5vw solid white';
 }
 /* Mn */
 document.getElementById('Mn-link').onmouseover = function() {
     document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
     document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+    document.getElementById('back').style.borderBottom = '0vw solid white';
 }
 document.getElementById('Mn-link').onmouseout = function() {
     document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('back').style.borderBottom = '0.5vw solid white';
 }
 /* Cd */
 document.getElementById('Cd-link').onmouseover = function() {
     document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
     document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
+    document.getElementById('back').style.borderBottom = '0vw solid white';
 }
 document.getElementById('Cd-link').onmouseout = function() {
     document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
     document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('back').style.borderBottom = '0.5vw solid white';
+}
+/* back */
+document.getElementById('back').onmouseover = function () {
+    document.getElementById('Hg-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0vw solid white';
+}
+
+document.getElementById('back').onmouseout = function () {
+    document.getElementById('Hg-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Pb-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Mn-link').style.borderBottom = '0.5vw solid white';
+    document.getElementById('Cd-link').style.borderBottom = '0.5vw solid white';
 }
